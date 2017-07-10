3b6da08d7efc584b6544ba3fce24d9680433bab1
Paul McLellan <paulmclellan_00@yahoo.com>
2017-07-10 15:54:10 -0700

    Modified url generator bean to obtain base URL from properties file

--- src/main/java/com/pm/bench/resttest/config/SpringConfig.java
+++ src/main/java/com/pm/bench/resttest/config/SpringConfig.java
@@ -1,20 +1,24 @@
 package com.pm.bench.resttest.config;
 
+import java.text.MessageFormat;
 import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 @ComponentScan( "com.pm.bench.resttest" )
 public class SpringConfig
 {
 	@Bean( "urlGenerator" )
-	public Function<Integer, String> getUrlGenerator()
+	public Function<Integer, String> getUrlGenerator( @Value( "${pageRequest.url}" ) final String urlTemplate )
 	{
-		return (i) -> "http://resttest.bench.co/transactions/" + i + ".json";
+		return (i) -> MessageFormat.format( urlTemplate, i );
 	}
 	
 	@Bean
