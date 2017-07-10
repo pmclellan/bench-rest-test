3013fdb9214006ddfa717776520c7db4563078fa
Paul McLellan <paulmclellan_00@yahoo.com>
2017-07-10 14:16:29 -0700

    Added extra unit tests for TransactionDatSource

--- src/test/java/com/pm/bench/resttest/datasource/rest/TransactionDataSourceTest.java
+++ src/test/java/com/pm/bench/resttest/datasource/rest/TransactionDataSourceTest.java
@@ -2,6 +2,8 @@ package com.pm.bench.resttest.datasource.rest;
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withServerError;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 import java.math.BigDecimal;
@@ -14,6 +16,7 @@ import java.util.function.Function;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.client.response.DefaultResponseCreator;
@@ -23,7 +26,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.common.collect.Iterators;
-import com.pm.bench.resttest.datasource.rest.TransactionDataSource;
+import com.pm.bench.resttest.datasource.DataSourceException;
+import com.pm.bench.resttest.datasource.MalformedDataException;
 import com.pm.bench.resttest.transaction.Transaction;
 import com.pm.bench.resttest.transaction.TransactionPage;
 
@@ -91,6 +95,30 @@ public class TransactionDataSourceTest
 		Iterator<Transaction> data = dataSource.getData();
 		Iterators.advance( data, transactionCount );
 		mockServer.verify();
+	}
+
+	@Test( expected = MalformedDataException.class )
+	public void shouldThrowMalformedDataExceptionWhenJsonStringIsInvalid()
+	{
+		mockServer.expect( requestTo( URL_GENERATOR.apply( 1 ) ) ).andExpect( method( HttpMethod.GET ) )
+						.andRespond( withSuccess( "{ invalid-json }", MediaType.APPLICATION_JSON ) );
+		dataSource.getData();
+	}
+
+	@Test( expected = DataSourceException.class )
+	public void shouldThrowDataSourceExceptionWhenClientErrorCodeIsReturned()
+	{
+		mockServer.expect( requestTo( URL_GENERATOR.apply( 1 ) ) ).andExpect( method( HttpMethod.GET ) )
+						.andRespond( withStatus( HttpStatus.NOT_FOUND ) );
+		dataSource.getData();
+	}
+
+	@Test( expected = DataSourceException.class )
+	public void shouldThrowDataSourceExceptionWhenServerErrorCodeIsReturned()
+	{
+		mockServer.expect( requestTo( URL_GENERATOR.apply( 1 ) ) ).andExpect( method( HttpMethod.GET ) )
+						.andRespond( withServerError() );
+		dataSource.getData();
 	}
 
 	private DefaultResponseCreator withTransactionPage( int totalCount, int pageNumber, int numTransactions )
