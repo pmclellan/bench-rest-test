014fdc6a5aafbfaffcb90f3bf85d5a09df18f75d
Paul McLellan <paulmclellan_00@yahoo.com>
2017-07-09 22:14:18 -0700

    Added PagedIterator and associated unit tests

---
+++ src/main/java/com/pm/bench/resttest/PagedIterator.java
@@ -0,0 +1,74 @@
+package com.pm.bench.resttest;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+public class PagedIterator<T> implements Iterator<T>
+{
+	private final int totalElements;
+	private final List<T> currentData;
+	private final PageLoader<T> pageLoader;
+	
+	private int currentPage = 1;
+	private int currentIndex = 0;
+	private int offset = 0;
+	
+	public PagedIterator(
+			int totalElements,
+			@NonNull final List<T> initialData,
+			@NonNull final PageLoader<T> pageLoader )
+	{
+		this.totalElements = totalElements;
+		this.currentData = new ArrayList<>( requireNonNull( initialData ) );
+		this.pageLoader = requireNonNull( pageLoader );
+	}
+	
+	@Override
+	public boolean hasNext() {
+		return currentIndex + offset < totalElements;
+	}
+
+	@Override
+	public T next() {
+
+		if ( !hasNext() )
+		{
+			throw new NoSuchElementException();
+		}
+		
+		if ( currentIndex >= currentData.size() )
+		{
+			loadNextPage();
+		}
+		
+		return currentData.get( currentIndex++ );
+	}
+	
+	private void loadNextPage()
+	{
+		List<T> newData = pageLoader.getPage( ++currentPage );
+		
+		if ( newData == null )
+		{
+			throw new RuntimeException( "Failed to load next page of data" );
+		}
+		
+		offset += currentIndex;
+		currentData.clear();
+		currentData.addAll( newData );
+		currentIndex = 0;
+	}
+	
+	public static interface PageLoader<T>
+	{
+		@Nullable
+		List<T> getPage( int pageNumber );
+	}
+}
---
+++ src/test/java/com/pm/bench/resttest/PagedIteratorTest.java
@@ -0,0 +1,90 @@
+package com.pm.bench.resttest;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.google.common.collect.Lists;
+import com.pm.bench.resttest.PagedIterator.PageLoader;
+
+@RunWith( MockitoJUnitRunner.class )
+public class PagedIteratorTest
+{
+	@Mock
+	private PageLoader<Integer> pageLoader;
+	
+	private PagedIterator<Integer> iterator;
+	
+	@Test( expected = NullPointerException.class )
+	public void shouldFailInstantiationWhenInitialDataIsNull()
+	{
+		new PagedIterator<>( 0, null, pageLoader );
+	}
+	
+	@Test( expected = NullPointerException.class )
+	public void shouldFailInstantiationWhenPageLoaderIsNull()
+	{
+		new PagedIterator<>( 0, new ArrayList<>(), null );
+	}
+	
+	@Test
+	public void shouldReturnFalseForHasNextWhenIteratorIsEmpty()
+	{
+		iterator = new PagedIterator<>( 0, new ArrayList<>(), pageLoader );
+		assertThat( iterator.hasNext(), is( false ) );
+	}
+	
+	@Test
+	public void shouldSupportIterationThroughSinglePageOfData()
+	{
+		List<Integer> data = Lists.newArrayList( 1, 2, 3 );
+		iterator = new PagedIterator<>( data.size(), data, pageLoader );
+		assertExpectedValues( iterator, data );
+	}
+	
+	@Test
+	public void shouldSupportIterationThroughMultiplePagesOfData()
+	{
+		List<Integer> data = Lists.newArrayList( 1, 2, 3, 4, 5, 6, 7 );
+		when( pageLoader.getPage( 2 ) ).thenReturn( data.subList( 3, 6 ) );
+		when( pageLoader.getPage( 3 ) ).thenReturn( data.subList( 6, 7 ) );
+		
+		iterator = new PagedIterator<>( data.size(), data.subList( 0, 3 ), pageLoader );
+		assertExpectedValues( iterator, data );
+	}
+	
+	@Test( expected = RuntimeException.class )
+	public void shouldThrowExceptionWhenPageLoadFails()
+	{
+		List<Integer> data = Lists.newArrayList( 1, 2, 3 );
+		when( pageLoader.getPage( 2 ) ).thenReturn( null );
+		
+		iterator = new PagedIterator<>( data.size() + 1, data, pageLoader );
+		
+		while ( iterator.hasNext() )
+		{
+			iterator.next();
+		}
+	}
+	
+	private void assertExpectedValues( final Iterator<Integer> iterator, List<Integer> expectedValues )
+	{
+		int index = 0;
+		
+		while ( iterator.hasNext() )
+		{
+			assertThat( iterator.next(), is( expectedValues.get( index++ ) ) );
+		}
+		
+		assertThat( index, is( expectedValues.size() ) );
+	}
+}
